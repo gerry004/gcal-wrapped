cba68b62042acdc337d390907882cacbb2ee4a0c
Gerry Yang <gerry04y@gmail.com>
2025-02-02 18:21:11 +0000

    title favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="min-h-screen p-8 bg-white flex items-center justify-center">
       <Navbar />
-      <div className="flex justify-center mt-8">
+      <div className="flex justify-center mt-8 w-[500px]">
         <InputForm />
       </div>
     </div>
